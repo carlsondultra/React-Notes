1a90f4218f2c5bbff5dd7728b9f10967bd75d094
carlsondultra <carlsondultra@users.noreply.github.com>
2021-10-13 22:58:36 -0400

    Passing deletetion of note through

--- src/App.js
+++ src/App.js
@@ -44,7 +44,10 @@ const deleteNote = (id) => {
 
   return (
     <div className="container">
-      <NotesList notes={notes} handleAddNote={addNote}/>
+      <NotesList 
+        notes={notes} 
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}/>
     </div>
   );
 };
